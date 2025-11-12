965b47a189334acc79c2a362c353a6bb4f670bbb
JuniorAlonso <alonsocb56@gmail.com>
2025-11-11 21:26:44 -0500

    Listooo

--- src/main/java/com/proyecto/Libreria/LibreriaApplication.java
+++ src/main/java/com/proyecto/Libreria/LibreriaApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.r2dbc.R2dbcAutoConfiguration; 
 
-// Añadir la exclusión
 @SpringBootApplication(exclude = {R2dbcAutoConfiguration.class})
 public class LibreriaApplication {
     public static void main(String[] args) {
